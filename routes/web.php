2da2f6026c355bb72fcf72f4a0f75cda7864a76e
Chris Martin <chrsmrtn81@gmail.com>
2021-03-11 19:18:04 +0000

    Test remote IP function

--- routes/web.php
+++ routes/web.php
@@ -22,7 +22,18 @@ Route::get('/environment-whitelist', function (Request $request) {
 Route::group(['middleware' => 'whitelist'], function () {
     
     Route::get('/', function () {
-        return view('welcome');
+        //return view('welcome');
+        foreach (array('HTTP_CLIENT_IP', 'HTTP_X_FORWARDED_FOR', 'HTTP_X_FORWARDED', 'HTTP_X_CLUSTER_CLIENT_IP', 'HTTP_FORWARDED_FOR', 'HTTP_FORWARDED', 'REMOTE_ADDR') as $key){
+            if (array_key_exists($key, $_SERVER) === true){
+                foreach (explode(',', $_SERVER[$key]) as $ip){
+                    $ip = trim($ip); // just to be safe
+                    if (filter_var($ip, FILTER_VALIDATE_IP, FILTER_FLAG_NO_PRIV_RANGE | FILTER_FLAG_NO_RES_RANGE) !== false){
+                        return $ip;
+                    }
+                }
+            }
+        }
+        return request()->ip(); // it will return server ip when no client ip found
     });
 
 });
