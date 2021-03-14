a4466173ad14be0e19ee99840234acde339603a2
Chris Martin <chrsmrtn81@gmail.com>
2021-03-14 17:00:16 +0000

    Update Fetch.php

--- app/Http/Controllers/Apis/OpenWeather/Fetch.php
+++ app/Http/Controllers/Apis/OpenWeather/Fetch.php
@@ -31,7 +31,7 @@ class Fetch extends Controller {
     public function multipleLocations()
     {
         $response = Http::get($this->url . 'group?', [
-            'id'=> '1609350,2636389,2642607,1607551,2655664,2643743',
+            'id'=> '1609350,2636389,2642607,1607551,2655664',
             'units' => 'metric',
             'appid' => env('OPENWEATHER_API_KEY'),
         ]);
