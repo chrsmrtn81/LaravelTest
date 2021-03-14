81330f8e33edb238f7a5abfedfbad593cc26fe9e
Chris Martin <chrsmrtn81@gmail.com>
2021-03-14 21:01:14 +0000

    Update home-page.blade.php

--- resources/views/pages/home-page.blade.php
+++ resources/views/pages/home-page.blade.php
@@ -8,8 +8,7 @@
     <title>Chris Martin</title>
 
     {{-- CSS --}}
-    <link rel="preload" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <link rel="preload" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous" as="style" onload="this.rel='stylesheet'">
 
     <style>
         @font-face {
