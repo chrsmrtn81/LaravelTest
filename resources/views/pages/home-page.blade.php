ca6a7e724b8d8f76315d5a5990fb7c8f63f5eff9
Chris Martin <chrsmrtn81@gmail.com>
2021-03-15 21:57:50 +0000

    Update home-page.blade.php

--- resources/views/pages/home-page.blade.php
+++ resources/views/pages/home-page.blade.php
@@ -1,10 +1,35 @@
+<?php
+//get the last-modified-date of this very file
+$lastModified=filemtime(__FILE__);
+//get a unique hash of this file (etag)
+$etagFile = md5_file(__FILE__);
+//get the HTTP_IF_MODIFIED_SINCE header if set
+$ifModifiedSince=(isset($_SERVER['HTTP_IF_MODIFIED_SINCE']) ? $_SERVER['HTTP_IF_MODIFIED_SINCE'] : false);
+//get the HTTP_IF_NONE_MATCH header if set (etag: unique file hash)
+$etagHeader=(isset($_SERVER['HTTP_IF_NONE_MATCH']) ? trim($_SERVER['HTTP_IF_NONE_MATCH']) : false);
+
+//set last-modified header
+header("Last-Modified: ".gmdate("D, d M Y H:i:s", $lastModified)." GMT");
+//set etag-header
+header("Etag: $etagFile");
+//make sure caching is turned on
+header('Cache-Control: public, max-age=31536000');
+
+//check if page has changed. If not, send 304 and exit
+if (@strtotime($_SERVER['HTTP_IF_MODIFIED_SINCE'])==$lastModified || $etagHeader == $etagFile)
+{
+    header("HTTP/1.1 304 Not Modified");
+    exit;
+}
+
+?>
+
 <!DOCTYPE html>
 
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 
 <head>
     <meta charset="utf-8">
-    <meta Cache-Control:public; max-age=31536000 >
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="icon" href="https://res.cloudinary.com/chrsmrtn81/image/upload/c_scale,h_120/v1615670923/Chris%20Martin/cm-monogram_yqo4en.svg" sizes="any" type="image/svg+xml">
     <title>Chris Martin</title>
